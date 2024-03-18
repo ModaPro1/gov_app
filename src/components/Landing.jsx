410905a1a35dfcdc6920a36318ee8c84074739c4
ModaPro1 <mahmoudfarghly519@gmail.com>
2024-03-18 15:56:02 +0200

    update

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,14 +2,19 @@ import classes from "./Landing.module.css";
 import arrow from '../assets/arrow.svg'
 import video from '../assets/bg-video.mp4'
 import { useTranslation } from "react-i18next";
+import { useEffect, useRef } from "react";
 
 export default function Landing() {
-
+  const videoEl = useRef()
   const {t} = useTranslation()
+
+  useEffect(() => {
+    videoEl.current.play()
+  }, [])
 
   return (
     <div className={classes.landing}>
-      <video className={classes.video} controls={false} autoPlay muted loop>
+      <video className={classes.video} controls={false} muted loop ref={videoEl}>
         <source src={video} type="video/mp4" />
         Your browser does not support HTML video.
       </video>
