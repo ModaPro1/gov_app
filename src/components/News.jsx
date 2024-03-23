eb5d4aa01e8b325de0630e3cdd9f2278b1ac9312
ModaPro1 <mahmoudfarghly519@gmail.com>
2024-03-24 00:00:43 +0200

    small design change

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -6,16 +6,18 @@ export default function News() {
       <MainTitle classes="border-gray">الأخبار</MainTitle>
       <div className="container">
         <div className="box mt-5 d-flex gap-5 position-relative">
-          <div className="controls d-flex gap-3">
-            <i class="fa-solid fa-arrow-right disabled"></i>
-            <i class="fa-solid fa-arrow-left disabled"></i>
+          <div className="position-relative">
+            <div className="controls d-flex gap-3">
+              <i class="fa-solid fa-arrow-right disabled"></i>
+              <i class="fa-solid fa-arrow-left disabled"></i>
+            </div>
+            <p className="mt-5">
+              الناظر / حسين محمد مضر الشريف نحنُ في طور الاستعداد والتأمين لموسم
+              حج 1445هـ . ونقوم بالتنسيق مع حكومة تيلانجانا والمسؤول التنفيذي
+              للبعثة الهندية للحج لإجراء القرعة الالكترونية في حيدر آباد لترشيح
+              الحجاج المستحقين بعناية وضمان جودة شفافية الاقتراع .
+            </p>
           </div>
-          <p className="mt-5">
-            الناظر / حسين محمد مضر الشريف نحنُ في طور الاستعداد والتأمين لموسم
-            حج 1445هـ . ونقوم بالتنسيق مع حكومة تيلانجانا والمسؤول التنفيذي
-            للبعثة الهندية للحج لإجراء القرعة الالكترونية في حيدر آباد لترشيح
-            الحجاج المستحقين بعناية وضمان جودة شفافية الاقتراع .
-          </p>
           <img
             src="/news/1.jpeg"
             alt="News"
